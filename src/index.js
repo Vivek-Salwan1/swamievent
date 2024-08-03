f330ffcfdc51dd31d969d70eebe2b3653f321aef
Vivek Salwan <viveksalwan63@gmail.com>
2024-08-03 19:42:18 +0530

    hashrouter added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import SpecialTours from './componanats/SpecialTours';
 import ContactUs from './componanats/ContactUs';
 import AboutUs from './componanats/AboutUs';
@@ -18,62 +18,29 @@ import InterCardDetails from './common/InterCardDetails';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const allRoutes  =createBrowserRouter(
-  [
-    {
-      path:'/',
-      element:<App/>
-    },
-    {
-      path:'specialTours',
-      element:<SpecialTours/>
+// Define routes using HashRouter
+const Routing = () => (
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="specialTours" element={<SpecialTours />} />
+      <Route path="aboutUs" element={<AboutUs />} />
+      <Route path="contactUs" element={<ContactUs />} />
+      <Route path="ArticleDetails/:articleID" element={<ArticleDetails />} />
+      <Route path="ServiceDetails/:serviceID" element={<ServiceDetails />} />
+      <Route path="allDomestic" element={<AllDomestic />} />
+      <Route path="allInternational" element={<AllInternational />} />
+      <Route path="termsAndConditions" element={<TermsAndConditions />} />
+      <Route path="PrivacyPolicy" element={<PrivacyPolicy />} />
+      <Route path="DomesticCardDetails/:state" element={<DomesticCardDetails />} />
+      <Route path="InterCardDetails/:country" element={<InterCardDetails />} />
+    </Routes>
+  </HashRouter>
+);
 
-    },
-    {
-      path:'aboutUs',
-      element:<AboutUs/>
-    },
-    {
-      path:'contactUs',
-      element:<ContactUs/>
-    },
-    {
-      path:'ArticleDetails/:articleID',
-      element:<ArticleDetails/>
-    },
-    {
-      path:'ServiceDetails/:serviceID',
-      element:<ServiceDetails/>
-    },
-    {
-      path:'allDomestic',
-      element:<AllDomestic/>
-    },
-    {
-      path:'allInternational',
-      element:<AllInternational/>
-    },
-    {
-      path:'termsAndConditions',
-      element:<TermsAndConditions/>
-    },
-    {
-      path:'PrivacyPolicy',
-      element:<PrivacyPolicy/>
-    },
-    {
-      path:'DomesticCardDetails/:state',
-      element:<DomesticCardDetails/>
-    },
-    {
-      path:'InterCardDetails/:country',
-      element:<InterCardDetails/>
-    }
-  ]
-)
-
+// Render the routing setup
 root.render(
-  <React.StrictMode>
-  <RouterProvider router={allRoutes}/> 
-  </React.StrictMode>
+
+    <Routing />
+
 );
